c46cc0a80bceca24ac610e67ddae05301cb55f8f
Rich Prohaska <prohaska@tokutek.com>
2014-10-29 08:44:10 -0400

    run tokudb rpl tests

--- run.mysql.tokudb.tests.bash
+++ run.mysql.tokudb.tests.bash
@@ -1,3 +1,3 @@
-for suite in tokudb_add_index tokudb_alter_table tokudb_bugs tokudb_mariadb tokudb ; do
-    ./mtr --suite=$suite --parallel=auto --force --retry=0 --max-test-fail=0 --testcase-timeout=60 --big-test --testcase-timeout=60 >$suite.out 2>&1
+for suite in tokudb_add_index tokudb_alter_table tokudb_bugs tokudb_mariadb tokudb rpl ; do
+    ./mtr --suite=plugin/tokudb/$suite --parallel=auto --force --retry=0 --max-test-fail=0 --testcase-timeout=60 --big-test --testcase-timeout=60 >$suite.out 2>&1
 done
